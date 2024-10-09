d3776164b2f5dedb4b63d364bc3b74059a4438e1
RodneyLoquero <rodneyidanan061302@gmail.com>
2024-10-09 16:48:35 +0800

    feat: I added a model of products in product dictionary and change the price of product number 4

--- productDict.py
+++ productDict.py
@@ -35,3 +35,26 @@ allProduct = (product1, product2, product3, product4, product5)
 
 for i, product in enumerate(allProduct):
     print(f"Product {i+1}: {product}")
+
+
+#Changing the price of product number four
+product4["price"] = 70
+print("\nUpdated Product 4:")
+print(product4)
+
+
+#Accessing the quantity of product number five
+print("\nQuantity of Product 5:", product5["quantity"])
+
+
+#Adding a model to the dictionary of products
+product1["model"] = "Dell XPS 15"
+product2["model"] = "Acer Nitro 5"
+product3["model"] = "Sony WH-1000XM3"
+product4["model"] = "Logitech G733"
+product5["model"] = "Razer DeathAdder"
+
+print("\nUpdated Products:")
+
+for i, product in enumerate(allProduct):
+    print(f"Product {i+1}: {product}")
